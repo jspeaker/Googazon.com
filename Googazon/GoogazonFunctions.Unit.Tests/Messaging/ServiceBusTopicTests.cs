c9b5f97396b211effc98d1ee70f3d8917845a3f7
jspeaker <james@iontech.org>
2019-12-15 15:11:27 -0800

    Moved Privateer to test conveniences project.

--- Googazon/GoogazonFunctions.Unit.Tests/Messaging/ServiceBusTopicTests.cs
+++ Googazon/GoogazonFunctions.Unit.Tests/Messaging/ServiceBusTopicTests.cs
@@ -5,8 +5,7 @@ using Microsoft.Azure.ServiceBus;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+using TestConveniences;
 
 namespace GoogazonFunctions.Unit.Tests.Messaging
 {
@@ -57,33 +56,6 @@ namespace GoogazonFunctions.Unit.Tests.Messaging
 
             // assert
             topicClient1.Should().NotBe(topicClient2);
-        }
-    }
-
-    public class Privateer
-    {
-        public T Object<T>(params object[] parameters) where T : class
-        {
-            ConstructorInfo constructor = typeof(T).GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, parameters.Select(p => p.GetType()).ToArray(), null);
-            if (constructor == null) throw new NullReferenceException($"Private constructor for type '{nameof(T)}' with the specified parameters was not found.");
-
-            return (T) constructor.Invoke(parameters);
-        }
-
-        public T Field<T>(object instance, string fieldName)
-        {
-            FieldInfo field = instance.GetType().GetField(fieldName, BindingFlags.Static | BindingFlags.GetField | BindingFlags.NonPublic | BindingFlags.Instance);
-            if (field == null) throw new NullReferenceException($"Private field '{fieldName}' for type '{nameof(T)}' was not found.");
-
-            return (T) field.GetValue(instance);
-        }
-
-        public void SetField<T>(object instance, string fieldName, T value)
-        {
-            FieldInfo field = instance.GetType().GetField(fieldName, BindingFlags.Static | BindingFlags.GetField | BindingFlags.NonPublic | BindingFlags.Instance);
-            if (field == null) throw new NullReferenceException($"Private field '{fieldName}' for type '{nameof(T)}' was not found.");
-
-            field.SetValue(instance, value);
         }
     }
 }
---
+++ Googazon/TestConveniences/Privateer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace TestConveniences
+{
+    public class Privateer
+    {
+        public T Object<T>(params object[] parameters) where T : class
+        {
+            ConstructorInfo constructor = typeof(T).GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, parameters.Select(p => p.GetType()).ToArray(), null);
+            if (constructor == null) throw new NullReferenceException($"Private constructor for type '{nameof(T)}' with the specified parameters was not found.");
+
+            return (T) constructor.Invoke(parameters);
+        }
+
+        public T Field<T>(object instance, string fieldName)
+        {
+            FieldInfo field = instance.GetType().GetField(fieldName, BindingFlags.Static | BindingFlags.GetField | BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field == null) throw new NullReferenceException($"Private field '{fieldName}' for type '{nameof(T)}' was not found.");
+
+            return (T) field.GetValue(instance);
+        }
+
+        public void SetField<T>(object instance, string fieldName, T value)
+        {
+            FieldInfo field = instance.GetType().GetField(fieldName, BindingFlags.Static | BindingFlags.GetField | BindingFlags.NonPublic | BindingFlags.Instance);
+            if (field == null) throw new NullReferenceException($"Private field '{fieldName}' for type '{nameof(T)}' was not found.");
+
+            field.SetValue(instance, value);
+        }
+    }
+}
