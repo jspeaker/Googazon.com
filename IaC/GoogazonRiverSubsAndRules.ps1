d3380c64bcb2a24eda0d9cf14c853de9fc3bb42e
Jim Speaker <jim@iontech.org>
2019-12-22 22:12:02 -0800

    Added IaC to source control

---
+++ IaC/GoogazonRiverSubsAndRules.ps1
@@ -0,0 +1,24 @@
+﻿$resourceGroup = 'Googazon'
+$namespace = 'Googazon-Rivers'
+
+$topic = 'customerservice'
+
+$subscription = 'callcenter'
+New-AzureRmServiceBusSubscription -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Name $subscription
+Remove-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name '$Default' -Force
+New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='contact'"
+
+$subscription = 'chat'
+New-AzureRmServiceBusSubscription -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Name $subscription
+Remove-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name '$Default' -Force
+New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='contact'"
+
+$subscription = 'email'
+New-AzureRmServiceBusSubscription -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Name $subscription
+Remove-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name '$Default' -Force
+New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='contact'"
+
+$subscription = 'brickandmortar'
+New-AzureRmServiceBusSubscription -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Name $subscription
+Remove-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name '$Default' -Force
+New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='contact'"
