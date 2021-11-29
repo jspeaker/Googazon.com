18cb4f45c10c4b9726934bc3aebf86c340a07553
Jim Speaker <jimspeaker@gmail.com>
2021-11-29 08:57:16 -0600

    Updates to get this up and running again.

--- IaC/GoogazonRiverSubsAndRules.ps1
+++ IaC/GoogazonRiverSubsAndRules.ps1
@@ -6,19 +6,19 @@ $topic = 'customerservice'
 $subscription = 'callcenter'
 New-AzureRmServiceBusSubscription -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Name $subscription
 Remove-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name '$Default' -Force
-New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='contact'"
+New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='customercontact'"
 
 $subscription = 'chat'
 New-AzureRmServiceBusSubscription -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Name $subscription
 Remove-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name '$Default' -Force
-New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='contact'"
+New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='customercontact'"
 
 $subscription = 'email'
 New-AzureRmServiceBusSubscription -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Name $subscription
 Remove-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name '$Default' -Force
-New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='contact'"
+New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='customercontact'"
 
 $subscription = 'brickandmortar'
 New-AzureRmServiceBusSubscription -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Name $subscription
 Remove-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name '$Default' -Force
-New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='contact'"
+New-AzureRmServiceBusRule -ResourceGroupName $resourceGroup -Namespace $namespace -Topic $topic -Subscription $subscription -Name Default -SqlExpression "need='customercontact'"
